ac8728618dd18250f75d137f684402f00cd51034
Akshat Shukla <xsg6969@gmail.com>
2025-09-12 13:21:38 +0530

    Add files via upload

---
+++ js/01_script.js
@@ -0,0 +1,7 @@
+// For now, nothing here. We'll add functionality later.
+console.log("Portfolio loaded successfully.");
+
+
+function toggleMenu() {
+    document.getElementById("navLinks").classList.toggle("active");
+}
